51811c537b4642b3db2a942b7ba3dd6c2aa15e45
Oluwapizzy <adeyinkapaul10@gmail.com>
2026-09-04 19:13:11 -0400

    Fix plural and stemmed synonym lookup in conflict search.

--- namex-solr-api/src/namex_solr_api/services/namex_solr/utils/__init__.py
+++ namex-solr-api/src/namex_solr_api/services/namex_solr/utils/__init__.py
@@ -32,6 +32,11 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """This module manages util methods for the NameX solr service."""
+from .analysis_helpers import (
+    analyze_stemmed_agro_tokens,
+    parse_stemmed_tokens,
+    synonym_members_in_name,
+)
 from .formatting_helpers import (
     DISTINCTIVE_COVERAGE_BOOST_WEIGHT,
     INITIALS_GROUP_BOOST_WEIGHT,
@@ -46,11 +51,6 @@ from .formatting_helpers import (
     prep_query_str_namex,
     remove_designation_tokens,
     strip_trailing_designations,
-)
-from .analysis_helpers import (
-    analyze_stemmed_agro_tokens,
-    parse_stemmed_tokens,
-    synonym_members_in_name,
 )
 from .namex_search_helper import namex_search
 from .synonym_helpers import get_synonyms
