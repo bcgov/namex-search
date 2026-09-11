6c79c8bb233e889c69546b8f1cbfa593d99664c9
Oluwapizzy <adeyinkapaul10@gmail.com>
2026-09-11 10:22:08 -0400

    code cleanup

--- namex-solr-api/tests/unit/test_initials_ranking_boost.py
+++ namex-solr-api/tests/unit/test_initials_ranking_boost.py
@@ -40,10 +40,6 @@ def group_values(items: list[dict]) -> list[list[str]]:
 LOCKED_CLAUSES = [
     ("J.R.M. INVESTMENTS", ["jrm", "investments"]),
     ("J R M INVESTMENTS", ["jrm", "investments"]),
-    ("J&R&M INVESTMENTS", ["jrm", "investments"]),
-    ("J & R & M INVESTMENTS", ["jrm", "investments"]),
-    ("J&R&M& INVESTMENTS", ["jrm", "investments"]),
-    ("J & R & M & INVESTMENTS", ["jrm", "investments"]),
     ("INVESTMENTS J R M", ["jrm", "investments"]),
     ("PACIFIC J R M INVESTMENTS", ["jrm", "pacific", "investments"]),
     ("H&H INVESTMENTS", ["hh", "investments"]),
